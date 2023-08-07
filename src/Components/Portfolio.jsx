cee132cdd0d716103be8ea65855c7511c7b1b599
Muhammad Wali Ahmad <120174482+mwaliahmad@users.noreply.github.com>
2023-08-07 16:40:27 +0000

    Add Projects Details

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
-    description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+    title: "Blood Donation Management System",
+    description: 
+      "Efficiently manage and streamline blood donation processes through a comprehensive C# based system",
+    url: "https://github.com/mwaliahmad/Blood-Donation-Management-System",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Fire Buddy",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Immerse players in a captivating platform-based gaming experience crafted using C# programming.",
+    url: "https://github.com/mwaliahmad/Fire-Buddy",
+  },
+  {
+    title: "Honey Store Website",
+    description:
+      "Experience the sweetness of nature with our online honey store website, offering a golden array of pure and delicious honey products.",
+    url: "https://github.com/mwaliahmad/Web",
   },
   {
     title: "My Resume Site",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Created from Microsoft's resume workshop and deployed to Azure. Includes my experience and design abilities.",
+    url: "https://github.com/mwaliahmad/Portfolio-Site",
   },
 ];
 
