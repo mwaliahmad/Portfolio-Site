d74262e391785595517931662c0f59f4860efb25
Muhammad Wali Ahmad <120174482+mwaliahmad@users.noreply.github.com>
2023-08-08 07:10:33 +0000

    Create Education Section

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -62,8 +62,16 @@ const Portfolio = () => {
       <h2 style={{ textAlign: "center" }}>Portfolio</h2>
       <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
         <div style={{ maxWidth: "40%", alignSelf: "center" }}>
-        <img 
-        src={image} style={{ height: "90%", width: "100%", objectFit: "cover", animation: "1s ease-out 0s 1 slideIn" }} />
+          <img
+            src={image}
+            alt={imageAltText}
+            style={{
+              height: "90%",
+              width: "100%",
+              objectFit: "cover",
+              animation: "1s ease-out 0s 1 slideIn",
+            }}
+          />
         </div>
         <div className="container">
           {projectList.map((project) => (
